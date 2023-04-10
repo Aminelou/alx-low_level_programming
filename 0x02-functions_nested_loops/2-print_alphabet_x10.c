90af7a85de5887b867c7378af676e7ef284f94c9
Aminelou <loufaniamine93@gmail.com>
2023-04-10 16:14:30 -0700

    libmyy

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - Prints 10 times  the alphabet.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int i;
+    int j;
+
+    for (i = 1;i <= 10;i++)
+    {
+	    for ( j = 97; j <= 122; j++)
+	    { _putchar (j);
+	    }
+	    _putchar ('\n');
+                          
+    }
+ 
+                   
+
+
+
+    return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+1 int _putchar(char c);
+2 void print_alphabet(void);
+3 void print_alphabet_x10(void);
+4 int _islower(int c);
+4 int _isalpha(int c);
+5 int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
