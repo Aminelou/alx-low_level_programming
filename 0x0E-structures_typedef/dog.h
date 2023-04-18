eca72362c7dc7eb696ea7c7de3829ffefb21ad2d
Aminelou <loufaniamine93@gmail.com>
2023-04-18 16:57:42 -0700

    changess

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,11 +6,9 @@
  * @name: dog's name
  * @age: dog's age
  * @owner: owner's name
- *
  * Description: just a lone dog struct in a big kitty world
  */
-
-struct dog 
+struct dog
 {
 	char *name;
 	float age;
